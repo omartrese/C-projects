38a66ecee5379c745b46072e3f87537bd6a6a0cb
omartrese <omarlayachi13@gmail.com>
2025-04-14 22:16:00 +0200

    Remove unused new_pid function and clean up code formatting

--- memoryManagementSim/mem_manage.c
+++ memoryManagementSim/mem_manage.c
@@ -4,17 +4,15 @@
 // #include "anbui.h"
 #include "mem_manage.h"
 
+#define RAM_SIZE 65536 // Total memory size in KB
+#define PAGE_SIZE 4096    // Size of each page in KB
+
+
 Process create_process(int id, int size);
 void assign_pages(Process *p, int num_free_frames);
 void show_page_table(Process *p);
 void free_process(int id);
 void show_memory();
-
-__pid_t new_pid(void)
-{
-    __pid_t pid = getpid();
-    return pid;
-}
 
 Process create_process(int id, int size)
 {
